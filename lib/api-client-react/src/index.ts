ab94982a531db910d4204605f50c01939a72120b
Clonzer <133519982+Clonzer@users.noreply.github.com>
2026-04-09 00:24:10 +0000

    fix: comment out missing generated api.schemas import

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -1,6 +1,8 @@
 export * from "./generated/api";
-export * from "./generated/api.schemas";
+// export * from "./generated/api.schemas";   // Commented out because generated file is missing on Render
+
 export { ApiError, customFetch } from "./custom-fetch";
+
 export {
   ACCESS_TOKEN_STORAGE_KEY,
   AUTH_TOKEN_CHANGE_EVENT,
